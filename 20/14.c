33816e3b7a56d7ec70319a9cc4ec6ce63fe05b67
pengxin <pengxin@newlandpayment.com>
2022-03-04 17:30:16 +0800

    complete 14

--- 20/14.c
+++ 20/14.c
@@ -3,34 +3,40 @@
 #include<string.h>
 
 struct char_string{
-	int index;
-	char buff[100];
+	char* buff;
 };
 
-void compare(struct char_string *a, struct char_string *b)
+void compare(char **a,char **b)
 {
-	int len_a = strlen(a->buff);
-	int len_b = strlen(b->buff);
+	int len_a = strlen(*a);
+	int len_b = strlen(*b);
 	int len = len_a > len_b ? len_b : len_a;
+	
 	for(int i = 0; i < len; i++)
 	{
-		if(((a->buff[i] >= 'a' ? a->buff[i] - 'a' : a->buff[i] - 'A') > 
-			(b->buff[i] >= 'a' ? b->buff[i] - 'a' : b->buff[i] - 'A')) 
-			&&(a->index < b->index)){
-				int exchange = a->index;
-				a->index = b->index;
-				b->index = exchange;
+		if(((*(*a + i) >= 'a' ? *(*a + i) - 'a' : *(*a + i) - 'A') < 
+			(*(*b + i) >= 'a' ? *(*b + i) - 'a' : *(*b + i) - 'A')) ){
 				return ;
-			}
+		}else if(((*(*a + i) >= 'a' ? *(*a + i) - 'a' : *(*a + i) - 'A') > 
+			(*(*b + i) >= 'a' ? *(*b + i) - 'a' : *(*b + i) - 'A')) ){
+				
+			char* exchange = *a;
+			*a = *b;
+			*b = exchange;
+			
+			return ;
 		}
+		
 	}
-	if(len_b < len_a && a->index < b->index)
+	if(len_b < len_a)
 	{
-		int exchange = a->index;
-		a->index = b->index;
-		b->index = exchange;
+		char* exchange = *a;
+		*a = *b;
+		*b = exchange;
+		
 		return ;
 	}
+	
 }
 
 int main(void)
@@ -41,31 +47,25 @@ int main(void)
 		printf("n =%d\n",n);
 		list = (struct char_string *)calloc(n, sizeof(struct char_string));
 		for(int i = 0; i < n; i++){
+			(list + i)->buff = (char *)calloc(100, sizeof(char));
 			scanf("%s", (list + i)->buff);
-			(list + i)->index = i;
 		}
 		printf("----------\n");
-		for(int i = 0; i < n; i++){
-			printf("%d ",(list + i)->index);
-		}
+		
 		for(int i = 0; i < n - 1; i++){
 			for(int j = 0; j < n - i -1; j++){
-				compare(list + j, list + j + 1);
+				printf("\n||%s  %s||\n",(list + j)->buff, (list + j + 1)->buff);
+				compare(&(list + j)->buff, &(list + j + 1)->buff);
+				printf("\n--%s  %s--\n",(list + j)->buff, (list + j + 1)->buff);
 			}
 		}
+		
 		printf("\n----------\n");
 		for(int i = 0; i < n; i++){
-			printf("%d ",(list + i)->index);
+			printf("%s\n",(list + i)->buff);
+			free((list + i)->buff);
 		}
-		printf("\n----------\n");
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				if((list + j)->index == i){
-					printf("%s\n",(list + j)->buff);
-				}
-			}
-		}
+		free(list);
 	}
-	free(list);
 	return 0;
 }
